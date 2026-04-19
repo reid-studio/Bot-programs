392b0d43efc5cf2082d02d6cd70b2aac21845445
reid-studio <reidsimpson2015@gmail.com>
2026-04-19 08:05:53 -0400

    Add welcome message to launching-program.py
    Added a welcome message to the launching program.

--- launching-program.py
+++ launching-program.py
@@ -1,6 +1,14 @@
 # This program launches the other main programs for easier use.
 import os
 
+print('''
+==============================
+Welcome to the Bot-programs.py
+==============================
+
+''')
+
+
 while true:
   cmd = input('Enter the name of the program you want to do, dont forget the .py! ')
 
